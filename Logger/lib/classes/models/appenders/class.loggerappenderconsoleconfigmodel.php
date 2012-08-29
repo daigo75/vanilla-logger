ec744f9f98a48434b328c5cdef2ad6f59913f35e
Diego Zanella <diza567@hotmail.com>
2012-08-29 01:01:09 +0000

    - Improved documentation.

--- Logger/lib/classes/models/appenders/class.loggerappenderconsoleconfigmodel.php
+++ Logger/lib/classes/models/appenders/class.loggerappenderconsoleconfigmodel.php
@@ -1,7 +1,18 @@
 <?php if (!defined('APPLICATION')) exit();
 
+/**
+ * Console Appender Configuration Model
+ * @package LoggerPlugin
+ */
 class LoggerAppenderConsoleConfigModel extends LoggerAppenderConfigModel {
-
+	/**
+	 * Expands the data contained in the Configuration field of an Appender
+	 * Configuration in the fields that form it.
+	 *
+	 * @param AppenderID The ID of the Appender for which to retrieve the
+	 * configuration.
+	 * @return An associative array of Configuration parameters.
+	 */
 	public function GetAppenderConfig($AppenderID) {
 		// Retrieve settings from configuration table
 		$Config = parent::GetAppenderConfig($AppenderID);
@@ -18,6 +29,13 @@ class LoggerAppenderConsoleConfigModel extends LoggerAppenderConfigModel {
 		return $Config;
 	}
 
+	/**
+	 * Transforms a set of posted fields into a JSON structure to be saved in
+	 * Appenders configuration table and saves them.
+	 *
+	 * @param FormPostValues An associative array of values posted by a form.
+	 * @return True if the values could be saved correctly, False otherwise.
+	 */
 	public function Save($FormPostValues) {
 		// Transforms posted values into an array to populate Configuration field in
 		// LoggerAppenders configuration table
