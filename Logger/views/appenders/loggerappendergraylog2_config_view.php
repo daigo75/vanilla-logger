dae7b181b0225ce92c9ea54b9fbc79ed71f3804e
Diego Zanella <diza567@hotmail.com>
2012-08-31 23:16:26 +0000

    - Added/Modified licensing terms.

--- Logger/views/appenders/loggerappendergraylog2_config_view.php
+++ Logger/views/appenders/loggerappendergraylog2_config_view.php
@@ -1,13 +1,23 @@
 <?php	if (!defined('APPLICATION')) exit();
-/*
-Copyright 2012 Diego Zanella IT Services
-This file is part of Logger Plugin for Vanilla Forums.
-
-Logger Plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version.
-Logger Plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Logger Plugin .  If not, see <http://www.gnu.org/licenses/>.
-
-Contact Diego Zanella at diego [at] pathtoenlightenment [dot] net
+/**
+ * Copyright 2012 Diego Zanella
+ * This file is part of Logger Plugin for Vanilla Forums.
+ *
+ * Plugin is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ * Plugin is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ * You should have received a copy of the GNU General Public License along with
+ * Logger Plugin. If not, see http://opensource.org/licenses/GPL-2.0.
+ *
+ * @package Logger Plugin
+ * @author Diego Zanella <diego@pathtoenlightenment.net>
+ * @copyright Copyright (c) 2011 Diego Zanella (http://dev.pathtoenlightenment.net)
+ * @license http://opensource.org/licenses/GPL-2.0 GPL 2.0
 */
 
 // Set default value for Graylog2 Port
