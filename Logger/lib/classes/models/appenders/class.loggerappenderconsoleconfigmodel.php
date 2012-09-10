ed8bbbcfda4411828a4ccfff92da9df0859a2bc7
Diego Zanella <businessdad75@gmail.com>
2012-09-10 14:59:14 +0100

    Modified Console Configuration Model to override the new DecodeAppenderParams and EncodeAppenderParams method.

--- Logger/lib/classes/models/appenders/class.loggerappenderconsoleconfigmodel.php
+++ Logger/lib/classes/models/appenders/class.loggerappenderconsoleconfigmodel.php
@@ -6,44 +6,22 @@
  */
 class LoggerAppenderConsoleConfigModel extends LoggerAppenderConfigModel {
 	/**
-	 * Expands the data contained in the Configuration field of an Appender
-	 * Configuration in the fields that form it.
-	 *
-	 * @param string AppenderID The ID of the Appender for which to retrieve the
-	 * configuration.
-	 * @return array An associative array of Configuration parameters\.
+	 * @see DecodeAppenderParams::LoggerAppenderConfigModel()
 	 */
-	public function GetAppenderConfig($AppenderID) {
-		// Retrieve settings from configuration table
-		$Config = parent::GetAppenderConfig($AppenderID);
-		if(empty($Config)) {
-			return false;
-		}
-
-		// Retrieve the settings encoded as JSON
-		$AppenderParams = json_decode($Config['Configuration'], TRUE);
-
-		$Config['Layout'] = $AppenderParams['layout']['class'];
-		$Config['Target'] = $AppenderParams['params']['target'];
-
-		return $Config;
+	protected function DecodeAppenderParams(array &$AppenderConfig, array $AppenderParams = null) {
+		$AppenderConfig['Layout'] = $AppenderParams['layout']['class'];
+		$AppenderConfig['Target'] = $AppenderParams['params']['target'];
 	}
 
 	/**
-	 * Transforms a set of posted fields into a JSON structure to be saved in
-	 * Appenders configuration table and saves them.
-	 *
-	 * @param array FormPostValues An associative array of values posted by a form.
-	 * @return bool True if the values could be saved correctly, False otherwise\.
+	 * @see DecodeAppenderParams::EncodeAppenderParams()
 	 */
-	public function Save($FormPostValues) {
+	protected function EncodeAppenderParams(array &$FormPostValues) {
 		// Transforms posted values into an array to populate Configuration field in
 		// LoggerAppenders configuration table
 		$Config = array('layout' => array('class' => $FormPostValues['Layout']),
 										'params' => array('target' => $FormPostValues['Target'],));
 
 		$FormPostValues['Configuration'] = json_encode($Config);
-
-		return parent::Save($FormPostValues);
 	}
 }
