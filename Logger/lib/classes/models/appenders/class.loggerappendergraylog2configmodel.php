16e71008b8b5e408a30166e7da8e4d9b39dc8666
Diego Zanella <businessdad75@gmail.com>
2012-09-10 14:56:43 +0100

    Modified Graylog2 Configuration Model to override the new DecodeAppenderParams and EncodeAppenderParams method.

--- Logger/lib/classes/models/appenders/class.loggerappendergraylog2configmodel.php
+++ Logger/lib/classes/models/appenders/class.loggerappendergraylog2configmodel.php
@@ -6,47 +6,23 @@
  */
 class LoggerAppenderGraylog2ConfigModel extends LoggerAppenderConfigModel {
 	/**
-	 * Expands the data contained in the Configuration field of an Appender
-	 * Configuration in the fields that form it.
-	 *
-	 * @param string AppenderID The ID of the Appender for which to retrieve the
-	 * configuration.
-	 * @return array An associative array of Configuration parameters\.
+	 * @see DecodeAppenderParams::LoggerAppenderConfigModel()
 	 */
-	public function GetAppenderConfig($AppenderID) {
-		// Retrieve settings from configuration table
-		$Config = parent::GetAppenderConfig($AppenderID);
-		if(empty($Config)) {
-			return false;
-		}
-
-		// Retrieve the settings encoded as JSON
-		$AppenderParams = json_decode($Config['Configuration'], TRUE);
-
-		$Config['HostName'] = $AppenderParams['params']['hostname'];
-		$Config['Port'] = $AppenderParams['params']['port'];
-		$Config['ChunkSize'] = $AppenderParams['params']['chunksize'];
-
-		return $Config;
+	protected function DecodeAppenderParams(array &$AppenderConfig, array $AppenderParams = null) {
+		$AppenderConfig['HostName'] = $AppenderParams['params']['hostname'];
+		$AppenderConfig['Port'] = $AppenderParams['params']['port'];
+		$AppenderConfig['ChunkSize'] = $AppenderParams['params']['chunksize'];
 	}
 
 	/**
-	 * Transforms a set of posted fields into a JSON structure to be saved in
-	 * Appenders configuration table and saves them.
-	 *
-	 * @param array FormPostValues An associative array of values posted by a form.
-	 * @return bool True if the values could be saved correctly, False otherwise\.
+	 * @see DecodeAppenderParams::EncodeAppenderParams()
 	 */
-	public function Save($FormPostValues) {
-		// Transforms posted values into an array to populate Configuration field in
-		// LoggerAppenders configuration table
+	protected function EncodeAppenderParams(array &$FormPostValues) {
 		$Config = array('params' => array('hostname' => $FormPostValues['HostName'],
 																			'port' => $FormPostValues['Port'],
 																			'chunksize' => $FormPostValues['ChunkSize'],
 																			));
 
 		$FormPostValues['Configuration'] = json_encode($Config);
-
-		return parent::Save($FormPostValues);
 	}
 }
