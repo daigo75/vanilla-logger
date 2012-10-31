b34b978a8f5f454ae72e09c78c928a77d6c11536
Diego Zanella <businessdad75@gmail.com>
2012-10-31 18:17:37 +0000

    Renamed Pro version to Advanced

--- Logger/class.logger.plugin.php
+++ Logger/class.logger.plugin.php
@@ -15,8 +15,8 @@ require(PATH_PLUGINS . '/Logger/lib/external/log4php/Logger.php');
 // Plugin definition
 $PluginInfo['Logger'] = array(
 	'Name' => 'Logger',
-	'Description' => 'Logger for Vanilla',
-	'Version' => '12.10.30',
+	'Description' => 'Advanced Logger for Vanilla',
+	'Version' => '12.10.31',
 	'RequiredApplications' => array('Vanilla' => '2.0.10'),
 	'RequiredTheme' => FALSE,
 	'RequiredPlugins' => FALSE,
