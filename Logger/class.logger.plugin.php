36466dcb5428ec851c802c4ea17a54bb6ecf6b0c
Diego Zanella <businessdad75@gmail.com>
2013-02-01 01:50:40 +0000

    Updated version and change log

--- Logger/class.logger.plugin.php
+++ Logger/class.logger.plugin.php
@@ -17,7 +17,7 @@ require(PATH_PLUGINS . '/Logger/lib/external/log4php/Logger.php');
 $PluginInfo['Logger'] = array(
 	'Name' => 'Logger',
 	'Description' => 'Logger for Vanilla - Basic Version',
-	'Version' => '13.01.04',
+	'Version' => '13.02.01',
 	'RequiredApplications' => array('Vanilla' => '2.0.10'),
 	'RequiredTheme' => FALSE,
 	'RequiredPlugins' => FALSE,
